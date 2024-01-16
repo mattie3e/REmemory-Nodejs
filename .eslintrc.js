5c933d47c5129cd37e898fba72857de131ff09d3
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-16 21:47:27 +0900

    feat: prttier, eslint 세팅 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,20 @@
 module.exports = {
-    extends: ["airbnb-base", "plugin:node/recommended", "prettier"],
-    parserOptions: {
-      sourceType: "module",
-    },
-    rules: {
-      "node/no-unsupported-features/es-syntax": ["error", { version: ">=14.0.0", ignores: ["modules"] }],
-    },
-  };  
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:node/recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { version: ">=14.0.0", ignores: ["modules"] },
+    ],
+    "no-nested-ternary": 0,
+    "prettier/prettier": "error",
+  },
+};
