3575cdd856ce4abe2c43e5b87c9a973ca78fe38f
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-19 16:14:51 +0900

    refactor: 파일 구조 변경에 따른 코드 수정

--- src/app/pcapsule/pcapsuleDao.js
+++ src/app/pcapsule/pcapsuleDao.js
@@ -1,65 +1,30 @@
-import { pool } from "../../../config/dbConfig.js";
-
-export const createPcs_d = async (data) => {
-	const {
-		pcapsule_name,
-		open_date,
-		dear_name,
-		theme,
-		content_type,
-		text_image_id,
-		voice_id,
-		capsule_number,
-	} = data;
-	const conn = await pool.getConnection();
-
-	// time_capsule 테이블에 capsule_number가 이미 존재하는지 확인
-	const [confirmTimeCapsule] = await conn.query(
-		`SELECT EXISTS(SELECT 1 FROM time_capsule WHERE capsule_number = ?) as isExistCapsule;`,
-		[capsule_number],
-	);
-
-	if (confirmTimeCapsule[0].isExistCapsule) {
-		conn.release();
-		return -1;
-	}
-
-	await conn.query(`INSERT INTO time_capsule (capsule_number) VALUES (?);`, [
-		capsule_number,
-	]);
-
-	const result = await conn.query(
-		`INSERT INTO pcapsule (capsule_number, pcapsule_name, open_date, dear_name, theme, content_type, text_image_id, voice_id) VALUES (?,?,?,?,?,?,?,?);`,
-		[
-			capsule_number,
-			pcapsule_name,
-			open_date,
-			dear_name,
-			theme,
-			content_type,
-			text_image_id,
-			voice_id,
-		],
-	);
-
-	conn.release();
-	return result;
+export const insertPcapsule = async (connection, data) => {
+	const query = `INSERT INTO pcapsule (capsule_number, pcapsule_name, open_date, dear_name, theme, content_type, text_image_id, voice_id) VALUES (?,?,?,?,?,?,?,?);`;
+	const [insertPcapsuleRow] = await connection.query(query, data);
+	return insertPcapsuleRow[0];
 };
 
-export const createCapsuleNum_d = async (nickname) => {
-	const conn = await pool.getConnection();
-	while (true) {
-		const random_number = Math.floor(Math.random() * 100000 + 1); // 1~100000 사이의 랜덤 숫자
-		const capsule_number = `${nickname}_${random_number}`;
+export const insertCapsuleNumber = async (connection, capsule_number) => {
+	const query = `INSERT INTO time_capsule (capsule_number) VALUES (?);`;
+	const [insertTimeCapsuleRow] = await connection.query(query, capsule_number);
+	return insertTimeCapsuleRow[0];
+};
 
-		const [confirmTimeCapsule] = await conn.query(
-			`SELECT EXISTS(SELECT 1 FROM time_capsule WHERE capsule_number = ?) as isExistCapsule;`,
-			[capsule_number],
-		);
+export const checkCapsuleNum_d = async (connection, capsule_number) => {
+	const query = `SELECT EXISTS(SELECT 1 FROM time_capsule WHERE capsule_number = ?) as isExistCapsule;`;
+	const [checkCapsuleNumRow] = await connection.query(query, capsule_number);
+	return checkCapsuleNumRow[0].isExistCapsule;
+};
 
-		if (!confirmTimeCapsule[0].isExistCapsule) {
-			conn.release();
-			return capsule_number;
-		}
-	}
+// 이건 공통으로 사용될거같은데 공통으로 사용되는 함수들 파일 필요할듯
+export const saveTextImage = async (connection, body, image_url, sort) => {
+	const query = `INSERT INTO text_image (body, image_url, sort) VALUES (?, ?, ?);`;
+	const [result] = await connection.query(query, [body, image_url, sort]);
+	return result.insertId;
+};
+
+export const saveVoice = async (connection, voice_url) => {
+	const query = `INSERT INTO voice (voice_url) VALUES (?);`;
+	const [result] = await connection.query(query, voice_url);
+	return result.insertId;
 };
--- src/app/pcapsule/pcapsuleService.js
+++ src/app/pcapsule/pcapsuleService.js
@@ -1,9 +1,17 @@
+import { pool } from "../../../config/dbConfig.js";
 import { BaseError } from "../../../config/error.js";
 import { status } from "../../../config/responseStatus.js";
+import { createCapsuleNum_p, createContent_p } from "./pcapsuleProvider.js";
+import {
+	checkCapsuleNum_d,
+	insertPcapsule,
+	insertCapsuleNumber,
+} from "./pcapsuleDao.js";
 
-import { createPcs_d, createCapsuleNum_d } from "./pcapsuleDao.js";
-
-export const createPcs_s = async (body, memeberId) => {
+// 캡슐 생성
+export const createPcs_s = async (body, nickname) => {
+	const { pcapsule_name, open_date, dear_name, theme, content_type, content } =
+		body;
 	const requiredFields = [
 		"pcapsule_name",
 		"open_date",
@@ -19,58 +27,45 @@ export const createPcs_s = async (body, memeberId) => {
 		}
 	});
 
-	// 사용자의 id로 DB에서 member 정보를 가져오는 함수
-	// User/Member의 Dao에서 작성된 파일있으면 가져오면 됨
-	const member = await getUserById(memeberId);
-	// capsule_number 생성
-	const capsule_number = createCapsuleNum_d(body.nickname);
+	const capsule_number = await createCapsuleNum_p(nickname);
 
-	const { pcapsule_name, open_date, dear_name, theme, content_type, content } =
-		body;
+	const { text_image_id, voice_id } = await createContent_p(
+		content_type,
+		content,
+	);
 
-	// content_type에 따라 text_image 또는 voice 테이블에 데이터 저장
-	// content는 보통 객체 또는 배열의 형태로 전달
-	// 예시
-	//   content: {
-	//     body: "글 내용",
-	//     image_url: "이미지 URL",
-	//     sort: "정렬 여부"
-	// },
-	// content: "음성 URL"
-	let text_image_id = null;
-	let voice_id = null;
+	const connection = await pool.getConnection(async (conn) => conn);
+	try {
+		connection.beginTransaction();
 
-	if (content_type === "text_image") {
-		const { body, image_url, sort } = content;
-		text_image_id = await saveTextImage(body, image_url, sort);
-	} else if (content_type === "voice") {
-		voice_id = await saveVoice(content);
-	}
+		const isExistCapsule = await checkCapsuleNum_d(connection, capsule_number);
 
-	const createPcsData = await createPcs_d({
-		pcapsule_name: pcapsule_name,
-		open_date: open_date,
-		dear_name: dear_name,
-		theme: theme,
-		content_type: content_type,
-		text_image_id: text_image_id,
-		voice_id: voice_id,
-		capsule_number,
-	});
+		if (isExistCapsule) {
+			connection.release();
+			throw new BaseError(status.CAPSULE_NOT_FOUND);
+		}
 
-	const NOT_EXIST = -1;
+		await insertCapsuleNumber(connection, capsule_number);
 
-	if (createPcsData == NOT_EXIST) {
-		throw new BaseError(status.CAPSULE_NOT_FOUND);
-	} else {
+		const insertData = [
+			capsule_number,
+			pcapsule_name,
+			open_date,
+			dear_name,
+			theme,
+			content_type,
+			content_type === 1 ? text_image_id : null,
+			content_type === 2 ? voice_id : null,
+		];
+		const createPcsData = await insertPcapsule(connection, insertData);
+
+		await connection.commit();
+
 		return { ...createPcsData, capsule_number };
+	} catch (error) {
+		await connection.rollback();
+		throw error;
+	} finally {
+		connection.release();
 	}
-
-	// const result = await createPcs_d(body);
-	// const NOT_EXIST = -1;
-	// if (result == NOT_EXIST) {
-	// 	throw new BaseError(status.CAPSULE_NOT_FOUND);
-	// } else {
-	// 	return result;
-	// }
 };
