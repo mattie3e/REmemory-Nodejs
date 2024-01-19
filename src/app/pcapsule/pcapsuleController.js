5dc1382d521d99bb107dcaecaa2b45188a6b710a
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-20 01:34:15 +0900

    refactor: userId 로직 변경

--- src/app/pcapsule/pcapsuleController.js
+++ src/app/pcapsule/pcapsuleController.js
@@ -1,21 +1,19 @@
 import { response } from "../../../config/response.js";
 import { status } from "../../../config/responseStatus.js";
 
-import { createPcs_s, updatePcs_s } from "./pcapsuleService.js";
+import { getUserInfos } from "../user/userService.js";
+
+import { createPcs_s } from "./pcapsuleService.js";
 
 // API Name : pcapsule 생성 API
 // [POST] /create
 export const createPcs_c = async (req, res, next) => {
 	// body: pcapsule_name, open_date, dear_name, theme, content_type, content
 	try {
-		// 사용자 인증
-		// const token = req.headers.authorization;
-		// 토큰으로부터 사용자의 id 추출하는 함수 (티티한테 관련된 함수 만들었는지 물어보기)
+		const userId = req.user.userId;
 
-		const nickname = getNicknameFromToken(token);
-
-		// 위에 부분은 User or Memeber의 Dao, Provider에서 가져오면 되지 않을까 하는 생각입니다.
-		// getNicknameFromToken 같은 함수!
+		const userInfos = await getUserInfos({ userId: userId });
+		const nickname = userInfos.data.nickname;
 
 		const data = await createPcs_s(req.body, nickname);
 		res.send(
--- src/app/pcapsule/pcapsuleRouter.js
+++ src/app/pcapsule/pcapsuleRouter.js
@@ -6,6 +6,4 @@ import { createPcs_c, updatePcs_c, readPcs_c } from "./pcapsuleController.js";
 export const pcapsuleRouter = express.Router();
 
 pcapsuleRouter.post("/create", asyncHandler(createPcs_c));
-// PATCH로 글+이미지 or 음성 받을때 update api 추가
-// pcapsuleRouter.patch("/create/:id", asyncHandler(updatePcs_c));
 // pcapsuleRouter.get("/retrieve", asyncHandler(readPcs_c));
