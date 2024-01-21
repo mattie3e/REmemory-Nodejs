6ec60df98c12f68b246fa0c141f2fa86c653395a
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-21 16:16:53 +0900

    feat: swagger 코드 추가

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import expressConfig from "./config/expressConfig.js";
 
+import { specs } from "./config/swagger.config.js";
+import SwaggerUi from "swagger-ui-express";
+
 import { userRouter } from "./src/app/User/userRoute.js";
 import { pcapsuleRouter } from "./src/app/Pcapsule/pcapsuleRoute.js";
 import { RcapsuleRouter } from "./src/app/Rcapsule/rcapsuleRoute.js";
 
 const app = expressConfig();
 const port = 3000;
+
+// swagger
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(specs));
 
 app.use("/user", userRouter);
 app.use("/pcapsule", pcapsuleRouter);
