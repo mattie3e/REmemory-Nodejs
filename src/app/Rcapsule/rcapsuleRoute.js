189141cbfff7485ea2d5cbd80db59336b237c7a0
nahyeon <k92544199@gmail.com>
2024-01-21 19:41:03 +0900

    fix: 오타 수정

--- src/app/Rcapsule/rcapsuleRoute.js
+++ src/app/Rcapsule/rcapsuleRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 
-import { createRcapsule } from "./rcapsuleController";
+import { createRcapsule } from "./rcapsuleController.js";
 
 export const rcapsuleRouter = express.Router();
 
