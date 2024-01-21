4ce329bb22df3c5db6691f2bccdc44f761c197a7
EthanolSwab <mmm6472@naver.com>
2024-01-21 20:27:12 +0900

    fix: 잘못 구현..

--- src/app/rcapsule/rcapsuleDao.js
+++ src/app/rcapsule/rcapsuleDao.js
@@ -1,16 +1,24 @@
 // dao.js
 
-//캡슐 번호 및 url 반환
-export const getRcs_d = async(capsule_number, capsule_url) => {
-    try {
+//캡슐 번호 및 url 조회
+export const readNumNUrl_d = async(connection, capsuleNumber) => {
         //삽입한 데이터의 id를 이용하여 조회
-        const [numNUrl] = await Connection.query(
-            'SELECT capsule_number, url FROM rcapsule WHERE id = ?;',
-            [rcapsule_id]
-        );
+        const query = 'SELECT capsule_number, url FROM rcapsule WHERE capsule_number = ?;'
+        const [resNumNurl] = await connection.query(query, [capsuleNumber]);
 
-        return numNUr[0]; //조회된 데이터 반환
-        }catch (error){
-            throw error;
+        //결과가 존재하지 않으면 null 반환 (캡슐 생성 X) * 추가
+        if (resNumNUrl.length === 0 ){
+            return null;
         }
+        //결과 반환
+        return resNumNurl[0];
 };
+
+// X
+// export const readCapsuleCnt_d = async(connection, rCapsuleNumber) => {
+//     const query = 'SELECT capsule_cnt FROM rcapsule WHERE capsule_number = ?'
+//     const resCnt = await connection.query(query, [rCapsuleNumber]);
+
+//     //결과 반환
+//     return { capsule_cnt: resCnt[0].capsule_cnt, };
+// }
