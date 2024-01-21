fd47487fd604c04c348fe0ab6e069d3b886bfcab
EthanolSwab <mmm6472@naver.com>
2024-01-21 20:19:42 +0900

    feat: url 접속 시 redirect 부분 추가

--- src/app/rcapsule/rcapsuleController.js
+++ src/app/rcapsule/rcapsuleController.js
@@ -1,7 +1,10 @@
 import { response } from "../../../config/response.js";
 import { status } from "../../../config/responseStatus.js";
 
-import { readNumnUrl_s } from "./rcapsuleService.js";
+import { readNumnUrl_s, 
+         readCapsuleCnt_s,
+         readDearNameNCnt_s 
+    } from "./rcapsuleService.js";
 /**
  * API Name : 캡슐번호, 롤링페이퍼 url 받기
  * [GET] : /rcapsule/info/:rcapsule_id
@@ -9,30 +12,56 @@ import { readNumnUrl_s } from "./rcapsuleService.js";
  */
 export const readNumNUrl_c = async (req, res, next) => {
     try{
-    //데이터 베이스에서 정보 조회 ... getRcs_s //연결..
+    //데이터 베이스에서 정보 조회
     const capsuleNumber = req.body.capsule_number;
 	const capsuleUrl = req.body.url;
 
-    const data = await getRcs_s(capsuleNumber,capsuleUrl); 
+    const data = await readNumnUrl_s(capsuleNumber,capsuleUrl); 
 
-    //받은 정보를 응답으로 보내기
-    res.send({numNUrl : data});
+    //받은 정보를 응답으로 보내기 .. status * 추가
+    res.json({numNUrl : data});
 
     } catch(error) { //에러 처리
         next(error);
-        console.log('Error: ${error.message}');
-        res.status(500).send(`Error: ${error.message}`);
+        console.log(error);
     }
 }
+/** 
+ * redirection 
+ * ./:rcapsule_number
+ */
+export const userUrl_c = async(req, res, next)=> {
+    //동적으로 변하는 값 (ex. wit_98765) 가져오기
+    const rCapsuleNumber = req.params.rcapsule_number;
+    try{
+        //rcapsuleNumber에 대한 정보를 조회하는 등의 작업 수행
+        // 새로운 URL 생성
+        //1) db에서 해당 rcapsule_cnt 조회
+        const rCapsuleCnt = await readCapsuleCnt_s(rCapsuleNumber);
+    
+        //2) 새로운 URL 생성
+        const redirectUrl = `/${rCapsuleNumber}/${rCapsuleCnt}`;
+        //3) redirection 수행
+        res.redirect(redirectUrl);
 
-// readNumUrl.listen(port, () => {
-//     console.log("rcapsule 캡슐 번호 및 롤링페이퍼 url 받기")
-// })
-
+    }catch (error){ // 에러 처리 ... *추가
+        next(error);
+        console.log(error);
+    }
+}
 /**
  * API NAME : url 들어왔을 시 화면
- * [GET] : /rcapsule_number
+ * dearName 과 rcapsule_id 반환
+ * [GET] : /:rcapsule_number/:rCapsuleCnt
  */
+export const readDearNameNCnt_c = async(req, res, next) => {
+    try {
+        
+    }catch (error){
+        next(error);
+        console.log(error);
+    }
+}
 
 
 /**
