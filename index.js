3ec344ba075aa086cf2f6404a622e548a9f7eb92
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-21 16:24:30 +0900

    fix: 오타수정

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import expressConfig from "./config/expressConfig.js";
 
-import { specs } from "./config/swagger.config.js";
+import { specs } from "./config/swaggerConfig.js";
 import SwaggerUi from "swagger-ui-express";
 
 import { userRouter } from "./src/app/User/userRoute.js";
 import { pcapsuleRouter } from "./src/app/Pcapsule/pcapsuleRoute.js";
-import { RcapsuleRouter } from "./src/app/Rcapsule/rcapsuleRoute.js";
+import { rcapsuleRouter } from "./src/app/Rcapsule/rcapsuleRoute.js";
 
 const app = expressConfig();
 const port = 3000;
@@ -15,7 +15,7 @@ app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(specs));
 
 app.use("/user", userRouter);
 app.use("/pcapsule", pcapsuleRouter);
-app.use("/rcapsule", RcapsuleRouter);
+app.use("/rcapsule", rcapsuleRouter);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
--- src/app/Rcapsule/rcapsuleRoute.js
+++ src/app/Rcapsule/rcapsuleRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 
-import { createRcapsule } from "./rcapsuleController";
+import { createRcapsule } from "./rcapsuleController.js";
 
 export const rcapsuleRouter = express.Router();
 
-rcapsuleRouter.post('/create', asyncHandler(createRcapsule));
+rcapsuleRouter.post("/create", asyncHandler(createRcapsule));
