3ab0233914d5f6f30372d4cb5761e55486f4fe32
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-21 14:16:24 +0900

    feat: pcapsule 조회 api 구현

--- src/app/pcapsule/pcapsuleRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import asyncHandler from "express-async-handler";
-
-import { createPcs_c, updatePcs_c, readPcs_c } from "./pcapsuleController.js";
-
-export const pcapsuleRouter = express.Router();
-
-pcapsuleRouter.post("/create", asyncHandler(createPcs_c));
-// pcapsuleRouter.get("/retrieve", asyncHandler(readPcs_c));
