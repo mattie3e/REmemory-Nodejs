417ca43fc651821efffbad3e97043a08c02f7411
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-21 14:13:27 +0900

    refactor: swagger.js apis 변경

--- config/swagger.js
+++ config/swagger.js
@@ -3,14 +3,14 @@ import SwaggerJsdoc from "swagger-jsdoc";
 const options = {
 	definition: {
 		info: {
-			title: "UMC Study API",
+			title: "RE:memory",
 			version: "1.0.0",
-			description: "UMC Study API with express, API 설명",
+			description: "Rememory API 설명",
 		},
 		host: "localhost:3000",
 		basepath: "../",
 	},
-	apis: ["./src/routes/*.js", "./swagger/*"],
+	apis: ["./src/app/*/Route.js", "./swagger/*.yaml"],
 };
 
 export const specs = SwaggerJsdoc(options);
