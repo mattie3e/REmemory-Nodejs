f3be974fc1a7aee5e24911ecd6f2138e70ea45a5
EthanolSwab <mmm6472@naver.com>
2024-01-25 15:54:25 +0900

    feat: 글/사진 쓰기

--- src/app/rcapsule/rcapsuleController.js
+++ src/app/rcapsule/rcapsuleController.js
@@ -3,9 +3,8 @@ import { status } from "../../../config/responseStatus.js";
 import baseResponse from "../../../config/response.js"; // 추가해야함
 
 import { readNumnUrl_s, 
-         //readCapsuleCnt_s,
          readDear_s,
-         creatText_s,
+         createText_s,
     } from "./rcapsuleService.js";
 /**
  * API Name : 캡슐번호, 롤링페이퍼 url 받기
@@ -28,29 +27,7 @@ export const readNumNUrl_c = async (req, res, next) => {
         console.log(error);
     }
 }
-/** X
- * redirection 
- * ./:rcapsule_number
- */
-// export const userUrl_c = async(req, res, next)=> {
-//     //동적으로 변하는 값 (ex. wit_98765) 가져오기
-//     const rCapsuleNumber = req.params.rcapsule_number;
-//     try{
-//         //rcapsuleNumber에 대한 정보를 조회하는 등의 작업 수행
-//         // 새로운 URL 생성
-//         //1) db에서 해당 rcapsule_cnt 조회
-//         const rCapsuleCnt = await readCapsuleCnt_s(rCapsuleNumber);
-    
-//         //2) 새로운 URL 생성
-//         const redirectUrl = `/${rCapsuleNumber}/${rCapsuleCnt}`;
-//         //3) redirection 수행
-//         res.redirect(redirectUrl);
 
-//     }catch (error){ // 에러 처리 ... *추가
-//         next(error);
-//         console.log(error);
-//     }
-// }
 /**
  * API NAME : url 들어왔을 시 화면
  * dearName 과 rcapsule_id 반환
@@ -74,26 +51,26 @@ export const readDear_c = async(req, res, next) => {
  * API NAME : 확인했어요! -> 글&사진 열기
  * [POST] : /rcapsule_number/text_photo
  */
-export const creatText_c = async(req, res, next) => {
-    //body : from_name, content_type, image, body
+export const createText_c = async(req, res, next) => {
+    //body : from_name, content_type, image_url, body
     try{
-        const {from_name, content_type, image, body} = req.body;
+        const {from_name, content_type, image_url, body} = req.body;
         
         //형식적 validation 처리
         if(!from_name){
             return res.send(errResponse(baseResponse.TEXTCAPSULE_FROM_NAME_EMPTY));
         } else if(!content_type){
             return res.send(errResponse(baseResponse.TEXTCAPSULE_CONTENT_TYPE_EMPTY));
-        } else if(!image && !body){
+        } else if(!image_url && !body){
             return res.send(errResponse(baseResponse.TEXTCAPSULE_IMAGEBODY_EMPTY));
         }        
         //처리 결과를 클라이언트에게 응답
-        const result = await creatText_sr(req.body);
+        const result = await createText_s(req.body);
 
         res.send(
             response(status.SUCCESS, {
                 ...req.body, // 원래의 데이터 복사
-                capsule_number: result.capsuleNumber, //새로운 필드 추가
+                capsule_number: result.capsuleNumber,
             })
         );
     }catch (error){ // * 추가
