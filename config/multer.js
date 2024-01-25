7479584abd0e76f22ebcb9fd3a461bcadc939d81
nahyeon <k92544199@gmail.com>
2024-01-25 15:48:34 +0900

    chore: 파일 업로드 기능 관련 패키지 설치

--- config/multer.js
+++ config/multer.js
@@ -1,7 +1,8 @@
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 import path from 'path';
-const AWS = require('./aws'); // aws.js 파일 가져오기
+// const AWS = require('./aws'); // aws.js 파일 가져오기
+import AWS from './aws';
 
 const upload = multer({
    storage: multerS3({
