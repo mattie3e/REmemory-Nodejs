febf26098acce31afd9bdf5168f12876353f623e
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-02-05 14:29:56 +0900

    Chore: Rcapsule 폴더명 수정

--- src/app/Rcapsule/rcapsuleRoute.js
+++ src/app/Rcapsule/rcapsuleRoute.js
@@ -6,6 +6,9 @@ import {
 	createRcapsule,
 	setRcapsulePw,
 	addVoiceLetter_c,
+  readNumNUrl_c,
+	readDear_c,
+	createText_c,
 } from "./rcapsuleController.js";
 
 export const rcapsuleRouter = express.Router();
@@ -14,6 +17,14 @@ rcapsuleRouter.post("/create", asyncHandler(createRcapsule));
 
 rcapsuleRouter.patch("/:rcapsule_id", asyncHandler(setRcapsulePw));
 
+//캡슐번호, 롤링페이퍼 url 받기
+rcapsuleRouter.get("/rcapsule/info", asyncHandler(readNumNUrl_c));
+
+//url 들어왔을 시
+rcapsuleRouter.get("/rcapsule_number", asyncHandler(readDear_c));
+
+//글&사진 쓰기
+rcapsuleRouter.post("/rcapsule_number/text_photo", asyncHandler(createText_c));
 // rcapsuleRouter.post(
 // 	"/:rcapsule_number/voice",
 // 	upload.single("voice_rcapsule"),
