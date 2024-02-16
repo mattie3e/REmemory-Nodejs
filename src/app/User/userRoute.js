b7a8d4fcc89ea001d8c9fa3f3c7db610a1e3f7f2
Taehyun An <ateahyun@naver.com>
2024-02-17 03:01:54 +0900

    [Fix] 닉네임 설정 api 통일

--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -13,9 +13,6 @@ export const userRouter = express.Router();
 // 회원가입, 로그인
 userRouter.get("/auth", asyncHandler(userSign));
 
-// 신규회원 닉네임 초기 설정
-userRouter.patch("/", asyncHandler(userSetNickname));
-
 // 회원정보 불러오기
 userRouter.get("/:userId", tokenAuthMiddleware, asyncHandler(userGetInfo));
 
