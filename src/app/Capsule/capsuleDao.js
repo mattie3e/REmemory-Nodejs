e490ec99e5909ccdd386b41790e8c2bfcd9d61bf
Taehyun An <ateahyun@naver.com>
2024-02-17 02:23:35 +0900

    Feat: capsule 통합 endpoint 추가, pcapsule query string 이름 변경

---
+++ src/app/Capsule/capsuleDao.js
@@ -0,0 +1,45 @@
+import { pool } from "../../../config/dbConfig";
+import { BaseError } from "../../../config/error";
+import { status } from "../../../config/responseStatus";
+
+export const getCapsule = async (userId) => {
+	try {
+		const conn = await pool.getConnection();
+
+		const getCapsules =
+			"SELECT time_capsule.capsule_number, rcapsule.rcapsule_name, pcapsule.pcapsule_name FROM time_capsule LEFT JOIN rcapsule ON time_capsule.capsule_number = rcapsule.capsule_number LEFT JOIN pcapsule ON time_capsule.capsule_number = pcapsule.capsule_number WHERE time_capsule.member_id = ?;";
+		const [capsules] = await pool.query(getCapsules, [userId]);
+
+		conn.release();
+		return capsules;
+	} catch (err) {
+		console.log(err);
+		throw new BaseError(status.BAD_REQUEST);
+	}
+};
+
+export const getCapsuleType = async (c_num) => {
+	try {
+		const conn = await pool.getConnection();
+
+		const query =
+			"SELECT EXISTS(SELECT 1 FROM time_capsule WHERE capsule_number = ?) as isExistCapsule;";
+		const [checkCapsuleNumRow] = await conn.query(query, c_num);
+
+		if (!checkCapsuleNumRow[0].isExistCapsule) {
+			conn.release();
+			return -1;
+		}
+
+		const typeP =
+			"SELECT EXISTS(SELECT 1 FROM pcapsule WHERE capsule_number = ?) as isExist";
+
+		const [pcapsule] = await conn.query(typeP, [c_num]);
+
+		conn.release();
+		return pcapsule[0].isExist;
+	} catch (err) {
+		console.log(err);
+		throw new BaseError(status.BAD_REQUEST);
+	}
+};
--- src/app/Capsule/capsuleRoute.js
+++ src/app/Capsule/capsuleRoute.js
@@ -1,16 +1,22 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import { tokenAuthMiddleware } from "../../../config/tokenAuthMiddleware";
-import { getCapsules, readCapsuleByType } from "./capsuleController";
+import {
+	getOwnCapsules,
+	readCapsuleByType,
+	readCapsuleDetail,
+} from "./capsuleController";
 
 export const capsuleRouter = express.Router();
 
-// 캡슐번호 조회 - p캡슐인지 r캡슐인지 구분할 수 있도록
+// 캡슐번호 조회
 capsuleRouter.get("/retrieve", asyncHandler(readCapsuleByType));
+
+capsuleRouter.get("/retrieve/detail", asyncHandler(readCapsuleDetail));
 
 // 유저의 전체 캡슐 가져오기
 capsuleRouter.get(
-	"retrieve/all",
+	"/retrieve/all",
 	tokenAuthMiddleware,
-	asyncHandler(getCapsules),
+	asyncHandler(getOwnCapsules),
 );
