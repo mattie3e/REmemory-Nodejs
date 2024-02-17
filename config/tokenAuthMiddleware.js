e54d478520261e1e198ca2d7c4b2e937f32fc6df
nahyeon <k92544199@gmail.com>
2024-02-18 04:12:38 +0900

    Test: tokenAuthMiddleware.js console.log 추가

--- config/tokenAuthMiddleware.js
+++ config/tokenAuthMiddleware.js
@@ -5,8 +5,10 @@ import { status } from "./responseStatus.js";
 export const tokenAuthMiddleware = (req, res, next) => {
 	try {
 		const header = req.headers["authorization"];
+		console.log('header', header);
 		const token = header && header.split(" ")[1];
-		console.log(header, token);
+		console.log('token', token);
+		// console.log(header, token);
 		if (token == null) return res.send(response(status.EMPTY_TOKEN));
 
 		jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
@@ -14,7 +16,7 @@ export const tokenAuthMiddleware = (req, res, next) => {
 				console.log(err);
 				return res.send(response(status.FORBIDDEN));
 			}
-			console.log(user);
+			console.log('user : ', user);
 			req.user = user;
 			next();
 		});
