90c55be5ff9efc39622d0453b0dc1dce5f448099
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-03-26 23:29:15 +0900

    Feat: db health check 추가

--- src/app/Health/healthRoute.js
+++ src/app/Health/healthRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { healthController } from "./healthController.js";
+import { healthController, healthDBController } from "./healthController.js";
 
 export const healthRoute = express.Router();
 
 healthRoute.get("", healthController);
+healthRoute.get("/db", asyncHandler(healthDBController));
