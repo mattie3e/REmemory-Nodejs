daf5f65d4f21d2831ecb55e496affe7f7f7c59c6
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-03-27 16:24:06 +0900

    Test: rcapsule 생성 오류 테스트

--- src/app/Rcapsule/rcapsuleDao.js
+++ src/app/Rcapsule/rcapsuleDao.js
@@ -210,6 +210,7 @@ export const saveTextImage_rcs = async (
 	image_url,
 	align_type,
 ) => {
+	console.log("현재 saveTextImage_rcs, 쿼리날리기 전");
 	const query = `INSERT INTO text_image (rwcapsule_id, body, image_url, align_type, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?);`;
 	const [result] = await connection.query(query, [
 		rwcapsule_id,
@@ -219,6 +220,12 @@ export const saveTextImage_rcs = async (
 		new Date(),
 		new Date(),
 	]);
+	console.log("현재 saveTextImage_rcs, 쿼리날린 후 result: ", result);
+	console.log("현재 saveTextImage_rcs, 쿼리날린 후 result: ", result[0]);
+	console.log(
+		"현재 saveTextImage_rcs, 쿼리날린 후 result: ",
+		result[0].insertId,
+	);
 	return result.insertId;
 };
 
