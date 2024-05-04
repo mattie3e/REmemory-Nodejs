2503cb4a30adb3f72abf7384b8135328be0e59b7
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-05-05 04:17:01 +0900

    Test: 비활성화 관련 테스트 코드

--- src/app/User/userController.js
+++ src/app/User/userController.js
@@ -25,10 +25,12 @@ export const userSign = async (req, res) => {
 	console.log("controller, userData 후");
 	const type = userData.type;
 	console.log("현재 controller, type: ", type);
+	console.log("userData.data: ", userData.data);
 	console.log("status: ", userData.data.status);
 
 	if (type == 1) {
 		if (userData.data.status === 0) {
+			console.log("전달값:", response(status.INACTIVE_ACCOUNT, userData.data));
 			res.status(200).send(response(status.INACTIVE_ACCOUNT, userData.data));
 		} else {
 			res.send(response(status.LOGIN_SUCCESS, userData.data));
